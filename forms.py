db35139a8d28a7cbbe87199f9138026b6b31a94b
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2020-11-01 22:14:47 +0530

    FInal Draft

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired
 
 class MNISTForm(FlaskForm):
     temp = SelectField(
-        'Temp Value',
+        'Category',
         choices=[('0', '0'), ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9')],
         validators=[DataRequired()]
     )
