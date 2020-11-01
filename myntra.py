95366b73b00c60fa237887964cf3e062fbc008b4
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2020-11-01 20:57:24 +0530

    UPDATE

--- myntra.py
+++ myntra.py
@@ -153,7 +153,7 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
+    picture_path = 'static/images/' + picture_fn
     form_picture.save(picture_path)
     return picture_path
 
